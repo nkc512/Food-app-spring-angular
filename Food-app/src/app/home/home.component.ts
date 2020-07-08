3e610d36079af244c06c7f9629ca00cbde63dcb3
snehal naikare <naikare.snehal13gmail.com>
2020-07-08 18:25:12 +0530

    CRUD and display dish according to restaurant is finished

--- Food-app/src/app/home/home.component.ts
+++ Food-app/src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Dish } from '../_classes/dish';
 import { Router } from '@angular/router';
 import { PublicService } from '../_services/public.service';
+import { Observable } from 'rxjs';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -9,21 +11,124 @@ import { PublicService } from '../_services/public.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  inputSearch: string;
-  dishes: Dish[];
-  constructor(private router: Router, private publicService: PublicService) { }
+  // inputSearch: string;
+  // dishes: Dish[];
+  // constructor(private router: Router, private publicService: PublicService) { }
 
-  ngOnInit(): void {
-    this.publicService.getAllDish().subscribe((dish: Dish[]) => {
-      this.dishes = dish;
-    });
+  // ngOnInit(): void {
+  //   this.publicService.getAllDish().subscribe((dish: Dish[]) => {
+  //     this.dishes = dish;
+  //   });
+  // }
+  // searchFood()
+  // {
+  //   this.publicService.getDish(this.inputSearch);
+  // }
+  // addToCart(dishId)
+  // {
+  //   // TODO
+  // }
+  head : HttpHeaders;
+  baseUrl : string= 'http://localhost:8080';
+  restaurantArray: String[]=[];
+  restaurantmsg: any;
+
+  constructor(private http: HttpClient, private publicservice: PublicService) {
+    
+    this.head = new HttpHeaders().set('access-control-allow-origin', this.baseUrl);
+   }
+
+  disharray: Dish[]=[];
+
+  categorySort: any;
+
+
+
+  ngOnInit() {
+    // this.categorySort = _.groupBy(this.disharray, function(dish) {
+    //   return dish.category;
+    // });
+    // console.log(this.categorySort,this.categorySort[0])
+    
+    // this.callGetAllDishes();
+    this.callGetDistinctRestaurant();
+  
+   
+
+
   }
-  searchFood()
-  {
-    this.publicService.getDish(this.inputSearch);
+   
+  CatagorySort(){
+    this.categorySort = this.disharray.reduce(function (r, a) {
+      r[a.category] = r[a.category] || [];
+      r[a.category].push(a);
+      return r;
+  }, Object.create(null));
+  console.log(this.categorySort);
+
   }
-  addToCart(dishId)
-  {
-    // TODO
+
+  get key(){
+    return Object.keys(this.categorySort);
+  }
+
+  getAllDishes(): Observable<Dish[]> {
+    return this.http.get<Dish[]>(this.baseUrl + '/dishdata/allDishes', { headers: this.head });
+  }
+
+ 
+
+  callGetDistinctRestaurant(){
+    this.publicservice.getDistinctRestaurant().subscribe(
+    response => {
+        console.log(response);
+        this.restaurantArray=response;
+        this.restaurantTab(this.restaurantArray[0]);
+        // this.restaurantmsg=this.restaurantArray[0];
+    },
+    err => {
+      console.log(err);
+    },
+    () => {
+      // this.CatagorySort();
+    }
+    );
+    }
+
+  // callGetAllDishes(){
+  // this.getAllDishes().subscribe(
+  // response => {
+  //     console.log(response);
+  //     this.disharray=response;
+  // },
+  // err => {
+  //   console.log(err);
+  // },
+  // () => {
+  //   this.CatagorySort();
+  // }
+  // );
+  // }
+
+
+  CallGetRestaurantDishes(restaurant:String){
+    this.publicservice.getRestaurantDishes(restaurant).subscribe(
+      response => {
+          console.log(response);
+          this.disharray=response;
+      },
+      err => {
+        console.log(err);
+      },
+      () => {
+        this.CatagorySort();
+      }
+      );
+  }
+
+  restaurantTab(restaurant:any){
+    console.log(restaurant);
+    this.restaurantmsg=restaurant;
+    this.CallGetRestaurantDishes(restaurant);
   }
 }
--- SpringFoodApp/src/main/java/com/example/demo/controller/TestController.java
+++ SpringFoodApp/src/main/java/com/example/demo/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +17,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.CafeteriaRepository;
+import com.example.demo.repository.DishRepository;
 import com.example.demo.sequence.SequenceGeneratorService;
+import com.mongodb.client.DistinctIterable;
+import com.mongodb.client.MongoCursor;
+import com.example.demo.model.Dish;;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -24,10 +31,15 @@ public class TestController {
 	@Autowired
 	CafeteriaRepository cafeteriaRepository;
 	
-
+	@Autowired
+    DishRepository dishRepository;
 	
 	@Autowired
 	SequenceGeneratorService sequenceGeneratorService;
+	
+	@Autowired
+	MongoTemplate mongoTemplate;
+	
 	@GetMapping("/all")
 	public String allAccess() {
 		return "Public Content.";
@@ -38,6 +50,28 @@ public class TestController {
 	public String userAccess() {
 		return "User Content.";
 	}
+	
+    @GetMapping("/getRestaurantDishes/{restaurantname}")
+    public ResponseEntity<List<Dish>> getRestaurantDishes(@PathVariable("restaurantname") String restaurantname) {
+    	System.out.println(dishRepository.findRstaurantDishes(restaurantname));
+    	
+        return ResponseEntity.ok().body(dishRepository.findRstaurantDishes(restaurantname));
+        
+    }
+    
+    @GetMapping("/allRestaurants")
+    public List<String> getRestaurants(){
+    	List<String> restaurantList = new ArrayList<>();
+    	DistinctIterable distinctIterable = mongoTemplate.getCollection("dish").distinct("restaurantName", String.class);
+    	MongoCursor cursor = distinctIterable.iterator();
+        while (cursor.hasNext()) {
+            String category = (String)cursor.next();
+            restaurantList.add(category);
+        }
+        
+        return  restaurantList;
+    	
+    }
 
 	
 //	@GetMapping("/")
